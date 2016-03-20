e2c9fe8df2b97ac119aa54dd61249973b9d1e430
alexkatz <alexanderikatz@gmail.com>
2016-03-19 21:15:59 -0400

    Initial Commit

---
+++ SampleCity/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleCity
+//
+//  Created by Alexander Katz on 3/19/16.
+//  Copyright © 2016 Alexander Katz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
